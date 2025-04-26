751e290a38c2bb503dfed1cdb90b1af8abda798c
simplicityf <omobolanlehazeezat@gmail.com>
2025-04-26 08:36:29 +0100

    Update the CI pipeline

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,5 +1,5 @@
-import { query, Router } from "express";
-import { body, param } from "express-validator";
+import { Router } from "express";
+import { body, param, query } from "express-validator";
 import {
   login,
   logout,
