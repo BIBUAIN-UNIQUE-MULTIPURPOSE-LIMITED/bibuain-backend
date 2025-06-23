cd26a320e4393e06155a68940356ffceed56bba4
simplicityf <omobolanlehazeezat@gmail.com>
2025-06-23 14:47:39 +0100

    fix: updated database url

--- src/config/database.ts
+++ src/config/database.ts
@@ -20,19 +20,14 @@ const isProd = process.env.NODE_ENV === "production";
 
 dotenv.config({ path: isProd ? ".env.production" : ".env" });
 
-const db_name = process.env.DB_NAME;
-const username = process.env.DB_USER;
-const password = process.env.DB_PASSWORD;
-const host = process.env.DB_HOST;
+const connectionUrl = process.env.DATABASE_URL!;
+if (!connectionUrl) {
+  throw new Error("Missing DATABASE_URL environment variable");
+}
 
 const dbConnect = new DataSource({
   type: "postgres",
-  host: `${host}`,
-  port: 5432,
-  username: username,
-  password: password,
-  database: db_name,
-  schema: "public",
+  url: connectionUrl,
   synchronize: false,
   logging: false,
   entities: [
